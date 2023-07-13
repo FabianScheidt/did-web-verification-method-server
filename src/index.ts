6776a2142de86ee7ef6260d1d4d4d13ad9265acb
Scheidt, Fabian <fabian.scheidt@accenture.com>
2023-07-13 14:59:54 +0200

    feat: add option to include root certificate in certificate chain

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
 import * as process from "process";
+import * as tls from "tls";
 import * as express from "express";
 import { Request, Response } from "express";
 import { JWK } from "node-jose";
+import { pki } from "node-forge";
 
+// Configuration
 if (!process.env.CERTIFICATE) {
   throw new Error("Environment Variable CERTIFICATE needs to be set!");
 }
 const CERT = process.env.CERTIFICATE;
 const PORT = process.env.PORT ?? 3000;
+const ADD_ROOT_CERT = process.env.ADD_ROOT_CERTIFICATE
+  ? process.env.ADD_ROOT_CERTIFICATE.toLowerCase() === "true"
+  : true;
+
+// Build list of available root certificates
+const ROOT_CERTS: pki.Certificate[] = [];
+for (const rootCert of tls.rootCertificates) {
+  try {
+    ROOT_CERTS.push(pki.certificateFromPem(rootCert));
+  } catch (e) {}
+}
 
 function getJwksDid(req: Request, res: Response): void {
   JWK.asKey(CERT, "pem").then((result) => {
@@ -47,7 +61,31 @@ function getJwksDid(req: Request, res: Response): void {
 }
 
 function getCert(req: Request, res: Response): void {
-  res.header("Content-Type", "application/x-pem-file").send(CERT);
+  if (!ADD_ROOT_CERT) {
+    res.header("Content-Type", "application/x-pem-file").send(CERT);
+    return;
+  }
+
+  // Extract certificates
+  const certsRegex =
+    /-----BEGIN CERTIFICATE-----[\s\S]+?-----END CERTIFICATE-----/g;
+  const certMatches = certsRegex[Symbol.matchAll](CERT);
+  const certs = [...certMatches].map((c) => c[0]);
+
+  // Iterate over certificates and try to find a matching root certificate
+  const outputCerts: string[] = [];
+  for (const cert of certs) {
+    outputCerts.push(cert);
+    const certObj = pki.certificateFromPem(cert);
+    const rootCert = ROOT_CERTS.find((r) => r.issued(certObj));
+    if (rootCert) {
+      outputCerts.push(pki.certificateToPem(rootCert));
+      break;
+    }
+  }
+
+  const payload = outputCerts.join("\n");
+  res.header("Content-Type", "application/x-pem-file").send(payload);
 }
 
 const app = express();

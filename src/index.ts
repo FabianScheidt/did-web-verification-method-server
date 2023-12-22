5bb3f6500588ddd390fbbcc1b5db6c54cdc5fe33
Scheidt, Fabian <fabian.scheidt@accenture.com>
2023-12-22 17:23:20 +0100

    feat: adjust setup to allow other packages to import methods

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,2 @@
-import * as process from "process";
-import * as express from "express";
-import { getDidDocumentHandler } from "./did-document";
-import { getCertificateChainHandler } from "./certificate-chain";
-
-if (!process.env.CERTIFICATE) {
-  throw new Error("Environment Variable CERTIFICATE needs to be set!");
-}
-const CERT = process.env.CERTIFICATE;
-const PORT = process.env.PORT ?? 3000;
-const ADD_ROOT_CERT = process.env.ADD_ROOT_CERTIFICATE
-  ? process.env.ADD_ROOT_CERTIFICATE.toLowerCase() === "true"
-  : true;
-
-const app = express();
-app.set("json spaces", 2);
-app.set("trust proxy", true);
-
-app.get("/**/did.json", getDidDocumentHandler(CERT));
-app.get(
-  "/**/certificate-chain.pem",
-  getCertificateChainHandler(CERT, ADD_ROOT_CERT),
-);
-
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+export * from "./certificate-chain";
+export * from "./did-document";
---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import * as process from "process";
+import * as express from "express";
+import { getDidDocumentHandler } from ".";
+import { getCertificateChainHandler } from ".";
+
+if (!process.env.CERTIFICATE) {
+  throw new Error("Environment Variable CERTIFICATE needs to be set!");
+}
+const CERT = process.env.CERTIFICATE;
+const PORT = process.env.PORT ?? 3000;
+const ADD_ROOT_CERT = process.env.ADD_ROOT_CERTIFICATE
+  ? process.env.ADD_ROOT_CERTIFICATE.toLowerCase() === "true"
+  : true;
+
+const app = express();
+app.set("json spaces", 2);
+app.set("trust proxy", true);
+
+app.get("/**/did.json", getDidDocumentHandler(CERT));
+app.get(
+  "/**/certificate-chain.pem",
+  getCertificateChainHandler(CERT, ADD_ROOT_CERT),
+);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
